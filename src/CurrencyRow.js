a92c2fd8591e697c27be0f9bcca0a86be17b4cd8
Snyeswa <53065926+Snyeswa@users.noreply.github.com>
2020-04-10 08:43:30 -0400

    setup the app

---
+++ src/CurrencyRow.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function CurrencyRow() {
+    return (
+        <div>
+            Currency Row
+            <input type='Number' />
+            <select>
+                <option value='Hi'>Hi</option>
+            </select>
+        </div>
+    )
+}
